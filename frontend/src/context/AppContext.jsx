ed84cf7cb8e8a1765186232d5eff324ca59a053f
Sahil Rawat <sahilr8587@gmail.com>
2025-04-22 22:31:42 +0530

    Add AppContext for global state management and update main rendering structure

---
+++ frontend/src/context/AppContext.jsx
@@ -0,0 +1,20 @@
+import { createContext, useContext, useState } from "react";
+
+export const AppContext = createContext();
+
+export const AppContextProvider = ({children}) =>{
+
+    const navigate = useNavigate();
+    const [user, setUser] = useState(null);
+    const [isSeller, setIsSeller] = useState(false);
+
+    const value = {navigate, User, setUser, setIsSeller, isSeller};
+
+    return <AppContext.Provider value={value}>
+        {children}
+    </AppContext.Provider>
+}
+
+export const useAppContext = () => {
+    return useContext(AppContext)
+}
