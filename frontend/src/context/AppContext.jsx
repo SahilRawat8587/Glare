9a0380066d23250d1a2ff68383e3a54f5d2c2b52
Sahil Rawat <sahilr8587@gmail.com>
2025-04-23 13:29:25 +0530

    Update Navbar component to include user authentication and improve mobile menu functionality

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -6,10 +6,12 @@ export const AppContext = createContext();
 export const AppContextProvider = ({children}) =>{
 
     const navigate = useNavigate();
-    const [user, setUser] = useState(null);
+    const [user, setuser] = useState(null);
     const [isSeller, setIsSeller] = useState(false);
+    const [showUserLogin, setshowUserLogin] = useState(false);
 
-    const value = {navigate, user, setUser, setIsSeller, isSeller};
+
+    const value = {navigate, user, setuser, setIsSeller, isSeller, showUserLogin, setshowUserLogin};
 
     return <AppContext.Provider value={value}>
         {children}
