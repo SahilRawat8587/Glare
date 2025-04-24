7945ccdbd333645580c31aa01027586a6abe847a
Sahil Rawat <sahilr8587@gmail.com>
2025-04-24 16:42:00 +0530

    Add Categories component and integrate it into Home page; update MainBanner with navigation links and enhance App structure to conditionally render Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
-import { Route, Routes } from 'react-router'
+import { Route, Routes, useLocation } from 'react-router'
 import Home from './pages/Home/Home'
 
 
 function App() {
 
+  const isSellerPath = useLocation().pathname.includes('/seller');
+
   return (
     <div>
-      <Navbar />
-      <div>
+      {isSellerPath ? null : <Navbar /> }
+      <div className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
         <Routes>
           <Route path='/' element={<Home />} />
         </Routes>
--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,6 +1,15 @@
 import logo from "./Logo (2).png";
 import main_banner from "./Banner_bg.png";
 import main_banner_mobile from "./Banner_bg_sm.png";
+import heels from "./White_Stiletto_High_Heel_Ankle_Strap_Sandals___30____liked_on_Polyvore-removebg-preview.png";
+import sneakers from "./download__4_-removebg-preview.png";
+import flats from "./Orcajump_-_Stylish_Cross-Strap_Sandals__Elegant_and_Comfy_Outdoor_Slippers_with_Faux_Hemp_Rope_Design-removebg-preview.png";
+import boots from "./Women_s_Chunky_Heel_Short_Boots__High_Heel_Pointed_Toe_Boots__Winter_Slim_Boots__British_Boots-removebg-preview.png";
+import loafers from "./Olivia_Mark_-_Peppy_Buckle_Heeled_Loafers_for_Women_s_Fashionable_Footwear-removebg-preview.png";
+import mules from "./Women_Mules_Designer_Pointed_Toe_Summer_Ladies_Fashion_Slippers_New_Comfort_Solid_Slides_Female-removebg-preview.png";
+import sliders from "./Vegan_Leather_Crinkled_Vamp_Sandals___SHEIN_USA-removebg-preview.png";
+import widges from "./BLACK_PLATFORM_WEDGE_SLIDE_HEELS_-_BLACK___6-removebg-preview.png";
+import ballet_flats from "./Soft_Leather_Ballet_Shoes_Ballet_Slippers_Dance_Shoes__Toddler_Little_Big_Kid_Women_-removebg-preview.png";
 
 
 export const assets = {
@@ -8,3 +17,60 @@ export const assets = {
     main_banner,
     main_banner_mobile,
 }
+
+export const categories = [
+    {
+        text: "Heels",
+        path: "heels",
+        image : heels,
+        bgColor: "#FEF6DA",
+    },
+    {
+        text: "Sneakers",
+        path: "sneakers",
+        image : sneakers,
+        bgColor: "#E7F2FF",
+    },
+    {
+        text: "Flats",
+        path: "flats",
+        image : flats,
+        bgColor: "#E7F2FF",
+    },
+    {
+        text: "Boots",
+        path: "boots",
+        image : boots,
+        bgColor: "#E7F2FF",
+    },
+    {
+        text: "Loafers",
+        path: "loafers",
+        image : loafers,
+        bgColor: "#E7F2FF",
+    },
+    {
+        text: "Mules",
+        path: "mules",
+        image : mules,
+        bgColor: "#E7F2FF",
+    },
+    {
+        text: "Sliders",
+        path: "sliders",
+        image : sliders,
+        bgColor: "#E7F2FF",
+    },
+    {
+        text: "Widges",
+        path: "widges",
+        image : widges,
+        bgColor: "#E7F2FF",
+    },
+    {
+        text: "Ballet Flats",
+        path: "ballet-flats",
+        image : ballet_flats,
+        bgColor: "#E7F2FF",
+    },
+]
