212f9bf9cfed984b5bf05c568dd865169da859e0
Sahil Rawat <sahilr8587@gmail.com>
2025-04-24 09:56:07 +0530

    Enhance Navbar component with user login/logout functionality and dropdown menu

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import { AlignJustify, Search, ShoppingCart } from 'lucide-react';
+import { AlignJustify, Search, ShoppingCart, UserRound } from 'lucide-react';
 import { assets } from '../assets/assets';
 import { useAppContext } from '../context/AppContext';
 
@@ -18,7 +18,7 @@ const Navbar = () => {
   return (
     <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
 
-    <NavLink to="/">
+    <NavLink to="/" onClick={() => setOpen(false)}>
         <img className="h-9" src={assets.logo} alt="GLARE Logo" />
     </NavLink>
 
@@ -33,14 +33,26 @@ const Navbar = () => {
             <Search className='w-4 h-4' />
         </div>
 
-        <div className="relative cursor-pointer">
+        <div onClick={() => navigate("/cart")} className="relative cursor-pointer">
             <ShoppingCart className='w-6 opacity-80' />
             <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">3</button>
         </div>
 
-        <button className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
+        {!user ? (
+        <button onClick={()=> setshowUserLogin(true)} className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
             Login
-        </button>
+        </button>)
+        :
+        (
+            <div className='relative group'>
+                <UserRound className='w-10 h-10 p-2' />
+                <ul className='hidden group-hover:block absolute top-10 right-0 bg-white shadow border border-gray-200 py-2.5 w-30 rounded-md text-sm z-40'>
+                    <li onClick={() => navigate("my-orders")} className='p-1.5 pl-3 hover:bg-primary/20 cursor-pointer'>My Orders</li>
+                    <li onClick={logout} className='p-1.5 pl-3 hover:bg-primary/20 cursor-pointer'>Logout</li>
+                </ul>
+
+            </div>
+        )}
     </div>
 
     <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" className="sm:hidden">
@@ -64,11 +76,14 @@ const Navbar = () => {
             <button onClick={()=>{
                 setOpen(false);
                 setshowUserLogin(true);
-            }} className="cursor-pointer px-6 py-2 mt-2  bg-primary hover:bg-primary-dull transition text-white rounded-full text-sm" onClick={()=> setOpen(false)}>
+            }} className="cursor-pointer px-6 py-2 mt-2  bg-primary hover:bg-primary-dull transition text-white rounded-full text-sm" >
             Login
         </button>
         ) : (
-            <button onClick={logout}  className="cursor-pointer px-6 py-2 mt-2  bg-primary hover:bg-primary-dull transition text-white rounded-full text-sm" onClick={()=> setOpen(false)}>
+            <button onClick={()=>{
+                logout();
+                setOpen(false);
+            }}  className="cursor-pointer px-6 py-2 mt-2  bg-primary hover:bg-primary-dull transition text-white rounded-full text-sm" >
             LogOut
         </button>
         ) }
