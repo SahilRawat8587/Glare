66ab41aeafd6fcff97c291c33a47c5099558929a
Sahil Rawat <sahilr8587@gmail.com>
2025-04-26 23:21:41 +0530

    Add related products section to ProductDetails page: display in-stock related products using ProductCard component

--- frontend/src/pages/ProductDetails/ProductDetails.jsx
+++ frontend/src/pages/ProductDetails/ProductDetails.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useAppContext } from "../../context/AppContext";
 import { Link, useParams } from "react-router-dom";
+import ProductCard from "../../components/ProductCard";
 
 const ProductDetails = () => {
 
@@ -90,6 +91,21 @@ const ProductDetails = () => {
                     </div>
                 </div>
             </div>
+            {/* Related Product */}
+            <div className="flex flex-col items-center mt-20 ">
+                <div className="flex flex-col items-end w-max ">
+                    <p className="font-medium text-3xl">Related Products</p>
+                    <div className="w-20 h-0.5 bg-primary rounded-full mt-2"></div>
+                </div>
+                <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md:gap-6 lg:grid-cols-5 mt-6 w-full">
+                    {relatedProducts.filter((product)=> product.inStock).map
+                    ((product, index) => (
+                        <ProductCard key={index} product={product} />
+                    ))}
+                </div>
+                <button onClick={()=> {navigate('/products'); scrollTo(0,0)}} className="mx-auto cursor-pointer px-12 my-16 py-2.5 border rounded text-primary hover:bg-primary/10 transition">See More</button>
+            </div>
+            
         </div>
     );
 };
